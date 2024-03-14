9062f91c179b1b9e6f26575611fb8aa6219a919f
N3TWORKL3G3ND <rojascastrojavier13@gmail.com>
2024-03-13 23:59:13 -0600

    Tercer programa

---
+++ 3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AreaDelTriangulo</title>
+</head>
+<body>
+    
+<?php
+
+$base = 600;
+$altura = 400;
+$medio = 0.5;
+
+echo "AREA = 1/2 x BASE x ALTURA<br>";
+echo "AREA = " . $medio . "x" . $base . "x" . $altura . "<br>";
+echo "AREA = " . $medio*$base*$altura;
+
+?>
+
+
+
+
+</body>
+</html>
